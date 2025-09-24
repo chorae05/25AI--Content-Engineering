5549926317ffa53daea91a641922dffc2bae20eb
chorae <chorae05@naver.com>
2025-09-24 14:49:25 +0900

    .

--- MySolution/4주차/main.c
+++ MySolution/4주차/main.c
@@ -2,21 +2,4 @@
 #include <math.h>
 #include <Windows.h>
 
-#define PI 3.14159265358979323846
 
-// 커서 이동 함수
-void moveCursor(int x, int y) {
-    printf("\x1b[%d;%dH", y, x);
-}
-
-// 폭탄 출력 함수
-void printBomb(int isExploded) {
-    if (isExploded) {
-        printf("\x1b[A^^^^^^^");
-        printf("\x1b[B\x1b[7D!!BAM!!");
-        printf("\x1b[B\x1b[7D^^^^^^^");
-    }
-    else {
-        printf("(  b  )");
-    }
-}
---
+++ MySolution/Array/main.c
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define Key_a 97
+#define Key_z 122
+
+void swap(char* a, char* b)
+{
+    char c = *a;
+    *a = *b;
+    *b = c;
+}
+
+int main()
+{
+    char random[Key_z - Key_a + 1];
+    char sort[Key_z - Key_a + 1];
+    int i;
+
+    // random 배열 초기화
+    for (i = 0; i < Key_z - Key_a + 1; i++) {
+        random[i] = 0;
+    }
+
+    // random 배열에 a~z 랜덤 배치
+    for (i = 0; i < Key_z - Key_a + 1; i++) {
+        while (1) {
+            int rand_index = rand() % (Key_z - Key_a + 1);
+            if (random[rand_index] == 0) {
+                random[rand_index] = (char)(i + Key_a);
+                break;
+            }
+        }
+    }
+
+    for (i = 0; i < Key_z - Key_a; i++) {
+        for (int j = 0; j < Key_z - Key_a - i; j++) {
+            if (random[j] > random[j + 1]) {
+                swap(&random[j], &random[j + 1]); // ✅ swap만 이용
+            }
+        }
+    }
+
+    
+    // swap 테스트
+    swap(&random[0], &random[1]);
+
+    
+    printf("\tindex\trandom\tsort");
+    for (i = 0; i < Key_z - Key_a + 1; i++) {
+        printf("\n\t%d\t%c", i, random[i] );
+    }
+
+    return 0;
+}
